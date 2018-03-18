0ccd283c0e988587674e7117f818f35539cb3b18
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-18 20:38:22 +0800

    add visualize

---
+++ svg_script/BlueprintCanvas.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.base
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
+
+Public Module BlueprintCanvas
+
+    <Extension>
+    Public Function Draw(solution As Routes, Optional bg$ = "white", Optional connectorStyle$ = Stroke.StrongHighlightStroke) As GraphicsData
+        Dim linePen As Pen = Stroke.TryParse(connectorStyle).GDIObject
+        Dim r! = 5
+        Dim plotInternal =
+            Sub(ByRef g As IGraphics, region As GraphicsRegion)
+
+                For Each anchor In solution.Anchors
+                    Call g.FillCircles(Brushes.Blue, {anchor.a}, r)
+                    Call g.FillCircles(Brushes.Blue, {anchor.b}, r)
+                Next
+
+                For Each connection In solution.PopulateRoutes
+                    For Each line In connection.SlideWindows(2)
+                        Dim a = line(0), b = line(1)
+                        Call g.DrawLine(linePen, a, b)
+                    Next
+                Next
+            End Sub
+
+        Return g.GraphicsPlots(solution.Size, New Padding(0), bg, plotInternal)
+    End Function
+
+    <Extension>
+    Public Iterator Function PopulateRoutes(solution As Routes) As IEnumerable(Of PointF())
+        Dim routesX = solution.X.Split(Function(v) v = -1.0R)
+        Dim routesY = solution.Y.Split(Function(v) v = -1.0R)
+
+        For i As Integer = 0 To solution.Anchors.Length - 1
+            Dim X = routesX(i)
+            Dim Y = routesY(i)
+
+            Yield X _
+                .Select(Function(px, pi) New PointF(px, Y(pi))) _
+                .ToArray
+        Next
+    End Function
+End Module
--- svg_script/test.vb
+++ svg_script/test.vb
@@ -5,12 +5,12 @@ Imports Microsoft.VisualBasic.MachineLearning.Darwinism.GAF.Helper
 Module test
 
     Sub Main()
-        Dim a = (New Point(10, 10), New Point(20, 20))
-        Dim b = (New Point(0, 0), New Point(25, 20))
-        Dim c = (New Point(15, 10), New Point(25, 25))
+        Dim a = (New Point(100, 100), New Point(2000, 2000))
+        Dim b = (New Point(500, 985), New Point(2500, 2000))
+        Dim c = (New Point(1500, 100), New Point(2500, 1850))
         Dim d = (a.Item1, b.Item1)
         Dim e = (c.Item1, b.Item1)
-        Dim size = New Size(25, 25)
+        Dim size = New Size(2600, 2500)
         Dim anchors = {a, b, c, d, e}
 
         Dim population As Population(Of Routes) = New Routes(anchors, size).InitialPopulation(1000)
@@ -19,8 +19,12 @@ Module test
         '   Dim out As New List(Of outPrint)
 
         ga.AddDefaultListener '(Sub(x) Call out.Add(x))
-        ga.Evolve(5000)
+        ga.Evolve(250)
         '   out.SaveTo("./outPrint.csv")
+
+        Dim solution = ga.Best
+
+        Call solution.Draw().Save("./test_print.png")
 
         Pause()
     End Sub
