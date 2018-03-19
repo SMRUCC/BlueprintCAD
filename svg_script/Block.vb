9e2bd7a38144bd7e11675bf1b77e01c9898ed4eb
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-19 21:07:03 +0800

    The routes blocker

---
+++ svg_script/Block.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging.Math2D
+
+''' <summary>
+''' The routes blocker
+''' </summary>
+Public MustInherit Class Block
+
+    Public MustOverride ReadOnly Property Location As PointF
+
+End Class
+
+Public Class rect : Inherits Block
+
+    Public ReadOnly rectangle As RectangleF
+
+    Public Overrides ReadOnly Property Location As PointF
+        Get
+            Return rectangle.Centre
+        End Get
+    End Property
+
+End Class
+
+Public Class Circle : Inherits Block
+
+    Public Property center As PointF
+    Public Property radius As Single
+
+    Public Overrides ReadOnly Property Location As PointF
+        Get
+            Return center
+        End Get
+    End Property
+
+End Class
