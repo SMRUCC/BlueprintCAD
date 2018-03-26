0e2cf3cd528d29c69abbf00facf4caa5d7c9a289
谢桂纲 <xie.guigang@gcmodeller.org>
2018-03-26 23:23:16 +0800

    add imports interface

---
+++ svg_script/GA/GA_AutoLayout.vb
@@ -0,0 +1,6 @@
+﻿Public Structure GA_AutoLayout
+
+    Public Function DoAutoLayout(Optional runs% = 5000) As Routes
+
+    End Function
+End Structure
---
+++ svg_script/KEGGImports.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Assembly.KEGG.WebServices
+
+''' <summary>
+''' Imports KEGG pathway reference map data.
+''' </summary>
+Module KEGGImports
+
+    <Extension>
+    Public Function ImportsMap(map As Map) As GA_AutoLayout
+
+    End Function
+End Module
