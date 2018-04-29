d9e0cf2aed0e58e6b4874fec0796cc1ce5562350
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-29 12:39:13 +0800

    get path property

---
+++ svg_script/GA/PathProperty.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm.base
+Imports Microsoft.VisualBasic.Imaging.Math2D
+
+''' <summary>
+''' 用于<see cref="Fitness"/>计算的一些路径的统计属性
+''' </summary>
+Public Class PathProperty
+
+    Public Property Length As Integer
+    Public Property TotalLength As Double
+    Public Property AverageLength As Double
+    Public Property MaxDelta As Double
+    Public Property MinDelta As Double
+    Public Property Path As PointF()
+
+    Sub New(path As Path)
+        Me.Path = path.Shrink.ToArray
+
+        ' 进行路径属性的计算
+        doStat()
+    End Sub
+
+    Private Sub doStat()
+        Dim dist = Path.SlideWindows(2) _
+                       .Select(Function(a, b) a.Distance(b)) _
+                       .ToArray
+
+        Length = Path.Length
+        TotalLength = dist.Sum
+        AverageLength = dist.Average
+        MaxDelta = dist.Max
+        MinDelta = dist.Min
+    End Sub
+End Class
