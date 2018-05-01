20f49fc1ff34821790dbf2657f16da5f49f47718
谢桂纲 <xie.guigang@gcmodeller.org>
2018-05-02 02:03:39 +0800

    Allocate path points

--- svg_script/GA/Models/Connector.vb
+++ svg_script/GA/Models/Connector.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.Drawing
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Math
 
 Namespace GA.Models
 
@@ -15,9 +18,43 @@ Namespace GA.Models
         ''' <param name="minSize%">最少的节点数量</param>
         ''' <param name="rand"></param>
         ''' <returns></returns>
+        ''' <remarks>
+        ''' 路径规定只有90度夹角
+        ''' </remarks>
         <Extension>
         Public Function Allocate(anchor As (A As Point, B As Point), minSize%, rand As Random) As Path
+            Dim points As New List(Of PointF)
+            Dim previous As PointF = anchor.A.PointF
+            Dim X, Y As Integer
+            Dim dx = anchor.B.X - anchor.A.X
+            Dim dy = anchor.B.Y - anchor.A.Y
 
+            For i As Integer = 1 To minSize - 1
+                Select Case rand.NextDouble
+                    Case < 0.5
+                        X = previous.X + dx * rand.NextDouble
+                        Y = previous.Y
+                    Case Else
+                        X = previous.X
+                        Y = previous.Y + dy * rand.NextDouble
+                End Select
+
+                points += New PointF(X, Y)
+                previous = points.Last
+            Next
+
+            ' 最后一个连接点
+            If rand.NextDouble < 0.5 Then
+                X = previous.X
+                Y = anchor.B.Y
+            Else
+                X = anchor.B.X
+                Y = previous.Y
+            End If
+
+            points += New PointF(X, Y)
+
+            Return New Path(anchor.A, anchor.B, points.X, points.Y)
         End Function
 
     End Module
