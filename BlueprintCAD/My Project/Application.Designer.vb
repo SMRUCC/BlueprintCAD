c57cb2b5cd7cfb03d9cbb59c229563a56e4332fe
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-08-26 11:05:00 +0800

    configs of the application profile

--- BlueprintCAD/My Project/Application.Designer.vb
+++ BlueprintCAD/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BlueprintCAD.Form1
+            Me.MainForm = Global.BlueprintCAD.FormEditor
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
