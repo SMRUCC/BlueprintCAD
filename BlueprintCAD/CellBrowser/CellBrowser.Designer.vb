a1d9e1a3fd4180d8fd603345e2925e0d76645e12
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-30 16:18:23 +0800

    truy to display the matrix

--- BlueprintCAD/CellBrowser/CellBrowser.Designer.vb
+++ BlueprintCAD/CellBrowser/CellBrowser.Designer.vb
@@ -22,11 +22,11 @@ Partial Class CellBrowser
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         MenuStrip1 = New MenuStrip()
         FileToolStripMenuItem = New ToolStripMenuItem()
         OpenVirtualCellDataFileToolStripMenuItem = New ToolStripMenuItem()
         PlotView1 = New ggviewer.PlotView()
-        GroupBox1 = New GroupBox()
         GroupBox2 = New GroupBox()
         SplitContainer2 = New SplitContainer()
         TreeView1 = New TreeView()
@@ -34,8 +34,12 @@ Partial Class CellBrowser
         Column1 = New DataGridViewLinkColumn()
         Column2 = New DataGridViewLinkColumn()
         SplitContainer1 = New SplitContainer()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        DataGridView2 = New DataGridView()
+        BindingSource1 = New BindingSource(components)
         MenuStrip1.SuspendLayout()
-        GroupBox1.SuspendLayout()
         GroupBox2.SuspendLayout()
         CType(SplitContainer2, ComponentModel.ISupportInitialize).BeginInit()
         SplitContainer2.Panel1.SuspendLayout()
@@ -46,6 +50,11 @@ Partial Class CellBrowser
         SplitContainer1.Panel1.SuspendLayout()
         SplitContainer1.Panel2.SuspendLayout()
         SplitContainer1.SuspendLayout()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(BindingSource1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' MenuStrip1
@@ -76,22 +85,11 @@ Partial Class CellBrowser
         PlotView1.Debug = True
         PlotView1.Dock = DockStyle.Fill
         PlotView1.ggplot = Nothing
-        PlotView1.Location = New Point(3, 19)
+        PlotView1.Location = New Point(3, 3)
         PlotView1.Name = "PlotView1"
         PlotView1.ScaleFactor = 1.25F
-        PlotView1.Size = New Size(1177, 528)
+        PlotView1.Size = New Size(1169, 516)
         PlotView1.TabIndex = 1
-        ' 
-        ' GroupBox1
-        ' 
-        GroupBox1.Controls.Add(PlotView1)
-        GroupBox1.Dock = DockStyle.Fill
-        GroupBox1.Location = New Point(0, 0)
-        GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(1183, 550)
-        GroupBox1.TabIndex = 2
-        GroupBox1.TabStop = False
-        GroupBox1.Text = "Dynamics Plot"
         ' 
         ' GroupBox2
         ' 
@@ -171,7 +169,7 @@ Partial Class CellBrowser
         ' 
         ' SplitContainer1.Panel1
         ' 
-        SplitContainer1.Panel1.Controls.Add(GroupBox1)
+        SplitContainer1.Panel1.Controls.Add(TabControl1)
         ' 
         ' SplitContainer1.Panel2
         ' 
@@ -179,6 +177,48 @@ Partial Class CellBrowser
         SplitContainer1.Size = New Size(1183, 832)
         SplitContainer1.SplitterDistance = 550
         SplitContainer1.TabIndex = 4
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Dock = DockStyle.Fill
+        TabControl1.Location = New Point(0, 0)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(1183, 550)
+        TabControl1.TabIndex = 0
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(PlotView1)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(1175, 522)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Dynamics Plot"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Controls.Add(DataGridView2)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(1175, 522)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "Matrix"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView2
+        ' 
+        DataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView2.Dock = DockStyle.Fill
+        DataGridView2.Location = New Point(3, 3)
+        DataGridView2.Name = "DataGridView2"
+        DataGridView2.Size = New Size(1169, 516)
+        DataGridView2.TabIndex = 0
         ' 
         ' CellBrowser
         ' 
@@ -192,7 +232,6 @@ Partial Class CellBrowser
         Text = "Cell Browser"
         MenuStrip1.ResumeLayout(False)
         MenuStrip1.PerformLayout()
-        GroupBox1.ResumeLayout(False)
         GroupBox2.ResumeLayout(False)
         SplitContainer2.Panel1.ResumeLayout(False)
         SplitContainer2.Panel2.ResumeLayout(False)
@@ -203,6 +242,11 @@ Partial Class CellBrowser
         SplitContainer1.Panel2.ResumeLayout(False)
         CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
         SplitContainer1.ResumeLayout(False)
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage2.ResumeLayout(False)
+        CType(DataGridView2, ComponentModel.ISupportInitialize).EndInit()
+        CType(BindingSource1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -211,7 +255,6 @@ Partial Class CellBrowser
     Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents OpenVirtualCellDataFileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents PlotView1 As ggviewer.PlotView
-    Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents SplitContainer1 As SplitContainer
     Friend WithEvents SplitContainer2 As SplitContainer
@@ -219,4 +262,9 @@ Partial Class CellBrowser
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Column1 As DataGridViewLinkColumn
     Friend WithEvents Column2 As DataGridViewLinkColumn
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents BindingSource1 As BindingSource
 End Class
