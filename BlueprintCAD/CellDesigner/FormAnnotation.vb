1c9fe4cd09ffb3b1434fb4be813c381b5bac8824
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-18 13:02:41 +0800

    update ui

--- BlueprintCAD/CellDesigner/FormAnnotation.vb
+++ BlueprintCAD/CellDesigner/FormAnnotation.vb
@@ -5,6 +5,14 @@ Public Class FormAnnotation
     Dim filepath As String
     Dim genbank As GBFF.File
 
+    Private Sub FormAnnotation_GotFocus(sender As Object, e As EventArgs) Handles Me.GotFocus
+        Workbench.SetFormActiveTitle(TabText)
+    End Sub
+
+    Private Sub FormAnnotation_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+        Workbench.RestoreFormTitle()
+    End Sub
+
     Public Function LoadModel(filepath As String) As FormAnnotation
         genbank = GBFF.File.Load(filepath)
         Return Me
