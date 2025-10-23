7b958484b54e0716a77f5cebc00cd4832d056314
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-23 23:59:02 +0800

    load registry data

--- src/CADRegistry/RegistryUrl.vb
+++ src/CADRegistry/RegistryUrl.vb
@@ -1,3 +1,6 @@
+Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+
 Public Class RegistryUrl
 
     ReadOnly server As String
@@ -5,5 +8,31 @@ Public Class RegistryUrl
     Sub New(server As String)
         Me.server = Strings.Trim(server).TrimEnd("/"c)
     End Sub
+
+    Public Function GetMoleculeDataById(id As UInteger) As WebJSON.Molecule
+        Dim url As String = $"{server}/registry/molecule/?id={id}"
+        Dim json_str As String = url.GET
+        Dim json As JsonObject = JsonParser.Parse(json_str)
+        Dim code As Integer = DirectCast(json!code, JsonValue)
+
+        If code <> 0 Then
+            Return Nothing
+        Else
+            Return json!info.CreateObject(Of WebJSON.Molecule)
+        End If
+    End Function
+
+    Public Function GetAssociatedReactions(ec_number As String) As Dictionary(Of String, WebJSON.Reaction)
+        Dim url As String = $"{server}/registry/reaction_network/?ec_number={ec_number}"
+        Dim json_str As String = url.GET
+        Dim json As JsonObject = JsonParser.Parse(json_str)
+        Dim code As Integer = DirectCast(json!code, JsonValue)
+
+        If code <> 0 Then
+            Return Nothing
+        Else
+            Return json!info.CreateObject(Of Dictionary(Of String, WebJSON.Reaction))
+        End If
+    End Function
 
 End Class
