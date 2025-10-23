5a84548f259d3c892eec30fbecc9d8b019941ca0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-23 22:40:58 +0800

    test project builder

--- src/CADRegistry/project_test/Program.vb
+++ src/CADRegistry/project_test/Program.vb
@@ -1,12 +1,18 @@
+Imports CADRegistry
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.BLASTOutput.BlastPlus
 Imports SMRUCC.genomics.Interops.NCBI.Extensions.Pipeline
 
 Module Program
     Sub Main(args As String())
-        Dim enzyme_hits = BlastpOutputReader _
+        Dim proj = ProjectCreator.FromGenBank(GBFF.File.Load("G:\BlueprintCAD\demo\Escherichia coli str. K-12 substr. MG1655.gbff"))
+
+        proj.enzyme_hits = BlastpOutputReader _
             .RunParser("G:\BlueprintCAD\demo\tmp\Sophia\19032\enzyme_blast650548.txt") _
             .ExportHistResult _
             .ToArray
+
+        proj.SaveZip("G:\BlueprintCAD\demo\ECOLI.gcproj")
 
         Pause()
     End Sub
