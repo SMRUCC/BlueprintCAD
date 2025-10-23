c70070014d2c8e28871ae82f4f2d3e7a9045fe93
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-23 23:50:13 +0800

    dd rest api json model

---
+++ src/CADRegistry/WebJSON/Molecule.vb
@@ -0,0 +1,7 @@
+﻿Namespace WebJSON
+
+    Public Class Molecule
+        Public Property name As String
+        Public Property formula As String
+    End Class
+End Namespace
---
+++ src/CADRegistry/WebJSON/Reaction.vb
@@ -0,0 +1,21 @@
+﻿Namespace WebJSON
+
+    Public Class Reaction
+        Public Property name As String
+        Public Property reaction As String
+        Public Property left As Substrate()
+        Public Property right As Substrate()
+        Public Property law As LawData()
+    End Class
+
+    Public Class Substrate
+        Public Property molecule_id As UInteger
+        Public Property factor As Double
+    End Class
+
+    Public Class LawData
+        Public Property params As Dictionary(Of String, String)
+        Public Property lambda As String
+        Public Property metabolite_id As String
+    End Class
+End Namespace
