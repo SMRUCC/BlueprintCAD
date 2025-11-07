e7a9e32b8eb47a8c0fe0aba26ebeee32bffbade5
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-08 02:53:41 +0800

    make improvements of thge debug view

--- src/CADRegistry/WebJSON/Reaction.vb
+++ src/CADRegistry/WebJSON/Reaction.vb
@@ -1,4 +1,6 @@
-﻿Namespace WebJSON
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Namespace WebJSON
 
     Public Class Reaction
         Public Property guid As String
@@ -7,16 +9,29 @@
         Public Property left As Substrate()
         Public Property right As Substrate()
         Public Property law As LawData()
+
+        Public Overrides Function ToString() As String
+            Return $"{guid} - {name}"
+        End Function
     End Class
 
     Public Class Substrate
         Public Property molecule_id As UInteger
         Public Property factor As Double
+
+        Public Overrides Function ToString() As String
+            Return $"{{{factor}}} {molecule_id}"
+        End Function
     End Class
 
     Public Class LawData
+
         Public Property params As Dictionary(Of String, String)
         Public Property lambda As String
         Public Property metabolite_id As String
+
+        Public Overrides Function ToString() As String
+            Return $"{params.GetJson} -> {lambda}"
+        End Function
     End Class
 End Namespace
