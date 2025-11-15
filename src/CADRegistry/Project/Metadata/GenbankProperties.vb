615e587a0d4be6b3e2315a9883fc95a8a8bec928
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-16 07:35:29 +0800

    add data model for project metadata

---
+++ src/CADRegistry/Project/Metadata/GenbankProperties.vb
@@ -0,0 +1,25 @@
+﻿Public Class GenbankProperties
+
+    Public Property organism_name As String = "unknown"
+    Public Property size As String
+    Public Property genes As Integer
+    Public Property proteins As Integer
+    Public Property operons As Integer
+    Public Property transcript_factors As Integer
+
+    Sub New()
+    End Sub
+
+    Sub New(proj As GenBankProject)
+        If Not proj.taxonomy Is Nothing Then
+            organism_name = proj.taxonomy.scientificName
+        End If
+
+        size = StringFormats.Lanudry(proj.nt.Length)
+        genes = proj.gene_table.TryCount
+        proteins = proj.proteins.TryCount
+        operons = proj.operons.TryCount
+        transcript_factors = proj.transcript_factors.TryCount
+    End Sub
+
+End Class
