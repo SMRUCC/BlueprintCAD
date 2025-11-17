f3f625687e03acb9a8c2c563b72c7b91c21b9ee8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-17 11:45:34 +0800

    add chart plot page

---
+++ src/BlueprintCAD/Pages/FormChartPad.Designer.vb
@@ -0,0 +1,58 @@
+﻿Imports Galaxy.ExcelPad
+Imports Galaxy.Workbench.DockDocument
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormChartPad
+    Inherits DocumentWindow
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PlotView1 = New ChartPad()
+        SuspendLayout()
+        ' 
+        ' PlotView1
+        ' 
+        PlotView1.BackColor = Color.White
+        PlotView1.BackgroundImageLayout = ImageLayout.Zoom
+        PlotView1.Dock = DockStyle.Fill
+        PlotView1.Location = New Point(0, 0)
+        PlotView1.Margin = New Padding(4, 4, 4, 4)
+        PlotView1.Name = "PlotView1"
+        PlotView1.Size = New Size(800, 450)
+        PlotView1.TabIndex = 1
+        ' 
+        ' FormChartPad
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(PlotView1)
+        DockAreas = Microsoft.VisualStudio.WinForms.Docking.DockAreas.Float Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockLeft Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockRight Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockTop Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockBottom Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.Document
+        DoubleBuffered = True
+        Name = "FormChartPad"
+        ShowHint = Microsoft.VisualStudio.WinForms.Docking.DockState.Unknown
+        TabPageContextMenuStrip = DockContextMenuStrip1
+        Text = "Chart Pad"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PlotView1 As ChartPad
+End Class
---
+++ src/BlueprintCAD/Pages/FormChartPad.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormChartPad
+
+End Class
