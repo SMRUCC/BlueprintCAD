bf4284dd56af8a54803a470106e63dcb06f3c2db
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-26 18:32:13 +0800

    try to config set of the cellular compnent content

--- src/BlueprintCAD/Runner/FormSetupCellularContents.Designer.vb
+++ src/BlueprintCAD/Runner/FormSetupCellularContents.Designer.vb
@@ -22,8 +22,125 @@ Partial Class FormSetupCellularContents
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        GroupBox1 = New GroupBox()
+        ListBox1 = New ListBox()
+        GroupBox2 = New GroupBox()
+        ListView1 = New ListView()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(ListBox1)
+        GroupBox1.Location = New Point(3, 3)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(174, 496)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Cell Colony"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.Dock = DockStyle.Fill
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(3, 19)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(168, 474)
+        ListBox1.TabIndex = 0
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(TextBox2)
+        GroupBox2.Controls.Add(Label2)
+        GroupBox2.Controls.Add(Button1)
+        GroupBox2.Controls.Add(TextBox1)
+        GroupBox2.Controls.Add(Label1)
+        GroupBox2.Controls.Add(ListView1)
+        GroupBox2.Location = New Point(183, 3)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(561, 493)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Set Content Data"
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Location = New Point(6, 22)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(549, 435)
+        ListView1.TabIndex = 0
+        ListView1.UseCompatibleStateImageBehavior = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(6, 466)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(45, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Search:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(57, 463)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(213, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(276, 462)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 3
+        Button1.Text = "Search"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(377, 466)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Content:"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(436, 463)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(119, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' FormSetupCellularContents
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "FormSetupCellularContents"
+        Size = New Size(747, 502)
+        GroupBox1.ResumeLayout(False)
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListView1 As ListView
 
 End Class
