2ecf2736913ec64e4aaf15f649406e2ee9614f94
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 04:09:53 +0800

    show startup page

--- src/BlueprintCAD/Pages/FormStartupPage.vb
+++ src/BlueprintCAD/Pages/FormStartupPage.vb
@@ -1,8 +1,13 @@
 ﻿Imports Galaxy.Workbench
+Imports Microsoft.Web.WebView2.Core
 
 Public Class FormStartupPage
 
     Private Async Sub FormStartupPage_Load(sender As Object, e As EventArgs) Handles Me.Load
         Await WebViewLoader.Init(WebView21, enableDevTool:=True)
+    End Sub
+
+    Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        Call WebView21.CoreWebView2.Navigate($"http://localhost:{AppEnvironment.WebPort}/index.html")
     End Sub
 End Class
