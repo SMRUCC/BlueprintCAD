d8efcb82ec9b6518235b8ddade038a12bb3aa900
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 05:12:43 +0800

    create viewer

--- src/BlueprintCAD/CellBrowser/ModelViewer/CellExplorer.Designer.vb
+++ src/BlueprintCAD/CellBrowser/ModelViewer/CellExplorer.Designer.vb
@@ -1,11 +1,12 @@
-﻿Imports Galaxy.Workbench.DockDocument
+﻿Imports Galaxy.Data.JSON
+Imports Galaxy.Workbench.DockDocument
 
 <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class CellExplorer
     Inherits ToolWindow
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -22,7 +23,7 @@ Partial Class CellExplorer
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         components = New ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CellExplorer))
@@ -30,6 +31,7 @@ Partial Class CellExplorer
         ToolStripLabel1 = New ToolStripLabel()
         ToolStripSpringTextBox1 = New Galaxy.CommonControls.ToolStripSpringTextBox()
         ToolStripButton1 = New ToolStripButton()
+        Panel1 = New Panel()
         ToolStrip1.SuspendLayout()
         SuspendLayout()
         ' 
@@ -62,16 +64,26 @@ Partial Class CellExplorer
         ToolStripButton1.Size = New Size(23, 22)
         ToolStripButton1.Text = "ToolStripButton1"
         ' 
+        ' Panel1
+        ' 
+        Panel1.Dock = DockStyle.Fill
+        Panel1.Location = New Point(0, 25)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(366, 658)
+        Panel1.TabIndex = 1
+        ' 
         ' CellExplorer
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(366, 683)
+        Controls.Add(Panel1)
         Controls.Add(ToolStrip1)
         DockAreas = Microsoft.VisualStudio.WinForms.Docking.DockAreas.Float Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockLeft Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockRight Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockTop Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockBottom Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.Document
         DoubleBuffered = True
         Name = "CellExplorer"
         ShowHint = Microsoft.VisualStudio.WinForms.Docking.DockState.Unknown
+        Text = "Cell Explorer"
         ToolStrip1.ResumeLayout(False)
         ToolStrip1.PerformLayout()
         ResumeLayout(False)
@@ -82,4 +94,5 @@ Partial Class CellExplorer
     Friend WithEvents ToolStripLabel1 As ToolStripLabel
     Friend WithEvents ToolStripSpringTextBox1 As Galaxy.CommonControls.ToolStripSpringTextBox
     Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents Panel1 As Panel
 End Class
--- src/BlueprintCAD/CellBrowser/ModelViewer/CellExplorer.vb
+++ src/BlueprintCAD/CellBrowser/ModelViewer/CellExplorer.vb
@@ -2,6 +2,7 @@
 Imports Galaxy.Data.JSON
 Imports Galaxy.Data.JSON.Models
 Imports Galaxy.Workbench
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualStudio.WinForms.Docking
 Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
 
@@ -17,6 +18,28 @@ Public Class CellExplorer
 
     Private Sub LoadCellComponents()
         Dim tree As New JsonObject With {.Id = model.cellular_id}
+        Dim metabolites As New JsonObject With {
+            .Id = "metabolites",
+            .JsonType = JsonType.Array,
+            .Parent = tree
+        }
+
+        If Not model.metabolismStructure?.compounds Is Nothing Then
+            Dim offset As i32 = 1
+
+            For Each meta As Compound In model.metabolismStructure.compounds
+                Dim obj As New JsonObject With {
+                    .Id = $"[{++offset}]",
+                    .JsonType = JsonType.Value,
+                    .Parent = metabolites,
+                    .Value = meta
+                }
+
+                Call metabolites.Fields.Add(obj)
+            Next
+        End If
+
+        Call tree.Fields.Add(metabolites)
 
         viewer.Tag = model.cellular_id
         viewer.Render(New JsonObjectTree(tree))
@@ -29,10 +52,11 @@ Public Class CellExplorer
 
     Private Sub CellExplorer_Load(sender As Object, e As EventArgs) Handles Me.Load
         TabText = "Cell Explorer"
+
         viewer = New JsonViewer
         viewer.Dock = DockStyle.Fill
 
-        Controls.Add(viewer)
+        Panel1.Controls.Add(viewer)
 
         ApplyVsTheme(ToolStrip1, viewer.GetContextMenu)
     End Sub
