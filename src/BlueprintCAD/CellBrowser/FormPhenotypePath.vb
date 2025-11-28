61ce81d43e318805d94f6b7c994f49d57e3c83c0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 17:29:41 +0800

    fix of the label diplsay

--- src/BlueprintCAD/CellBrowser/FormPhenotypePath.vb
+++ src/BlueprintCAD/CellBrowser/FormPhenotypePath.vb
@@ -1,21 +1,27 @@
-﻿Imports Galaxy.Workbench
+﻿Imports BlueprintCAD.UIData
+Imports Galaxy.Workbench
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Microsoft.VisualBasic.Data.visualize.Network.FileStream.Generic
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.IO
 
 Public Class FormPhenotypePath
 
     Dim g As NetworkGraph
     Dim vertex As Node()
     Dim qgram As QGramIndex
+    Dim view As NodeView()
 
-    Public Function LoadNetwork(network As Dictionary(Of String, FluxEdge), ByRef g As NetworkGraph) As FormPhenotypePath
+    Public Function LoadNetwork(network As Dictionary(Of String, FluxEdge),
+                                symbols As Dictionary(Of String, CompoundInfo),
+                                ByRef g As NetworkGraph) As FormPhenotypePath
         Dim offset As Integer
 
         If g Is Nothing Then
             g = TaskProgress.LoadData(streamLoad:=Function(bar As ITaskProgress)
-                                                      Return CreateNetwork(network, bar)
+                                                      Return CreateNetwork(network, symbols, bar)
                                                   End Function,
                                       title:="Initialize Data",
                                       info:="Build cellular network graph...",
@@ -25,11 +31,19 @@ Public Class FormPhenotypePath
         Me.g = g
         Me.qgram = New QGramIndex(6)
         Me.vertex = g.vertex.ToArray
+        Me.view = New NodeView(vertex.Length - 1) {}
 
         For Each v As Node In vertex
-            Call ListBox1.Items.Add(v.label)
-            Call ListBox2.Items.Add(v.label)
-            Call qgram.AddString(v.label, offset)
+            view(offset) = New NodeView With {
+                .id = v.label,
+                .name = v.data.label,
+                .type = v.data(NamesOf.REFLECTION_ID_MAPPING_NODETYPE),
+                .compartment = v.data("location")
+            }
+
+            Call ListBox1.Items.Add(view(offset))
+            Call ListBox2.Items.Add(view(offset))
+            Call qgram.AddString(view(offset).name, offset)
 
             offset += 1
         Next
@@ -37,7 +51,7 @@ Public Class FormPhenotypePath
         Return Me
     End Function
 
-    Private Shared Function CreateNetwork(network As Dictionary(Of String, FluxEdge), bar As ITaskProgress) As NetworkGraph
+    Private Shared Function CreateNetwork(network As Dictionary(Of String, FluxEdge), symbols As Dictionary(Of String, CompoundInfo), bar As ITaskProgress) As NetworkGraph
         Dim g As New NetworkGraph
         Dim n As Integer = network.Values.Count
         Dim d As Integer = n / 80
@@ -53,7 +67,12 @@ Public Class FormPhenotypePath
             Dim rxnNode As Node = g.GetElementByID(rxn.id)
 
             If rxnNode Is Nothing Then
-                g.CreateNode(rxn.id)
+                g.CreateNode(rxn.id, New NodeData With {
+                    .Properties = New Dictionary(Of String, String) From {
+                        {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, "BiologicalProcess"},
+                        {"location", "?"}
+                    }
+                })
                 rxnNode = g.GetElementByID(rxn.id)
             End If
 
@@ -61,33 +80,62 @@ Public Class FormPhenotypePath
                 Dim v As Node = g.GetElementByID(left.id)
 
                 If v Is Nothing Then
-                    g.CreateNode(left.id)
+                    Dim info As CompoundInfo = symbols.TryGetValue(left.id)
+                    Dim loc As String = If(left.compartment_id, "Unknown")
+
+                    g.CreateNode(left.id, New NodeData With {
+                        .Properties = New Dictionary(Of String, String) From {
+                            {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, "Molecule Entity"},
+                            {"location", loc}
+                        },
+                        .label = If(info Is Nothing, left.id, info.name)
+                    })
                     v = g.GetElementByID(left.id)
                 End If
 
                 If g.ExistEdge(rxnNode.label, v.label) OrElse g.ExistEdge(v.label, rxnNode.label) Then
                 Else
-                    g.CreateEdge(v, rxnNode)
+                    g.CreateEdge(v, rxnNode, 1, New EdgeData With {
+                        .Properties = New Dictionary(Of String, String) From {
+                            {NamesOf.REFLECTION_ID_MAPPING_INTERACTION_TYPE, "Substrate"}
+                        }
+                    })
                 End If
             Next
             For Each right As VariableFactor In rxn.right
                 Dim u As Node = g.GetElementByID(right.id)
 
                 If u Is Nothing Then
-                    g.CreateNode(right.id)
+                    Dim info As CompoundInfo = symbols.TryGetValue(right.id)
+                    Dim loc As String = If(right.compartment_id, "Unknown")
+
+                    g.CreateNode(right.id, New NodeData With {
+                        .Properties = New Dictionary(Of String, String) From {
+                            {NamesOf.REFLECTION_ID_MAPPING_NODETYPE, "Molecule Entity"},
+                            {"location", loc}
+                        },
+                        .label = If(info Is Nothing, right.id, info.name)
+                    })
                     u = g.GetElementByID(right.id)
                 End If
 
                 If g.ExistEdge(rxnNode.label, u.label) OrElse g.ExistEdge(u.label, rxnNode.label) Then
                 Else
-                    g.CreateEdge(rxnNode, u)
+                    g.CreateEdge(rxnNode, u, 1, New EdgeData With {
+                        .Properties = New Dictionary(Of String, String) From {
+                            {NamesOf.REFLECTION_ID_MAPPING_INTERACTION_TYPE, "Product"}
+                        }
+                    })
                 End If
             Next
 
             If (++tick) Mod d = 0 Then
-                Call bar.SetProgress(CInt(tick / n * 100), $"Build cellular network graph... {rxn.id}")
+                Call bar.SetProgress(CInt(tick / n * 100))
+                Call bar.SetInfo($"Build cellular network graph... {rxn.id}")
             End If
         Next
+
+        Call bar.TaskFinish()
 
         Return g
     End Function
---
+++ src/BlueprintCAD/UIModels/NodeView.vb
@@ -0,0 +1,15 @@
+﻿Namespace UIData
+
+    Public Class NodeView
+
+        Public Property id As String
+        Public Property name As String
+        Public Property type As String
+        Public Property compartment As String
+
+        Public Overrides Function ToString() As String
+            Return $"[{type}] {name}@{compartment}"
+        End Function
+
+    End Class
+End Namespace
