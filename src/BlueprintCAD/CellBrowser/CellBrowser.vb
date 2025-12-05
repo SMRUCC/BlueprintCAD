3fa4352cc90e2a6aae80bee25e72d5926101c5ab
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-05 17:59:17 +0800

    ignores of some common nodes

--- src/BlueprintCAD/CellBrowser/CellBrowser.vb
+++ src/BlueprintCAD/CellBrowser/CellBrowser.vb
@@ -738,7 +738,7 @@ Public Class CellBrowser
     Dim cache_graph As NetworkGraph = Nothing
 
     Private Sub OpenPhenotypeTool()
-        Call InputDialog.Input(config:=New FormPhenotypePath().LoadNetwork(network, symbols, {"WATER", "CO2"}, cache_graph))
+        Call InputDialog.Input(config:=New FormPhenotypePath().LoadNetwork(network, symbols, FormPhenotypePath.ignores, cache_graph))
     End Sub
 
     Private Sub CheckAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CheckAllToolStripMenuItem.Click
