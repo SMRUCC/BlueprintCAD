74a658aa0d05c42670159710f29065512451ad5d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-05 20:39:42 +0800

    run virtual cell at start page

--- src/BlueprintCAD/Pages/FormStartupPage.vb
+++ src/BlueprintCAD/Pages/FormStartupPage.vb
@@ -1,4 +1,6 @@
-﻿Imports Galaxy.Workbench
+﻿Imports System.Runtime.InteropServices
+Imports BlueprintCAD.My
+Imports Galaxy.Workbench
 Imports Microsoft.Web.WebView2.Core
 
 Public Class FormStartupPage
@@ -8,6 +10,15 @@ Public Class FormStartupPage
     End Sub
 
     Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        Call WebView21.CoreWebView2.AddHostObjectToScript("host", New HostObject)
         Call WebView21.CoreWebView2.Navigate($"http://localhost:{AppEnvironment.WebPort}/index.html")
     End Sub
+
+    <ComVisible(True)>
+    Public Class HostObject
+
+        Public Sub RunModel()
+            Call MyApplication.RunVirtualCell(Nothing, Nothing)
+        End Sub
+    End Class
 End Class
