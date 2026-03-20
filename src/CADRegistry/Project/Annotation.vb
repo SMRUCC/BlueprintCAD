2de1d341997d34c798176ace4d4f0991ac77133d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-20 16:13:28 +0800

    move model to cell builder project

--- src/CADRegistry/Project/Annotation.vb
+++ src/CADRegistry/Project/Annotation.vb
@@ -69,11 +69,3 @@ Public Module Annotation
 
 End Module
 
-Public Class ECNumberAnnotation
-
-    Public Property gene_id As String
-    Public Property EC As String
-    Public Property Score As Double
-    Public Property SourceIDs As String()
-
-End Class
