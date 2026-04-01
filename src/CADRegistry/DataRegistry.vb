8d5473d8614f4c3bb48b2235be04ce1a593f5aab
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-04-01 22:26:41 +0800

    data registry interface component

---
+++ src/CADRegistry/DataRegistry.vb
@@ -0,0 +1,12 @@
+﻿Imports CellBuilder
+
+Public Module DataRegistry
+
+    Public Function OpenRegistry(serverUrl As String) As IDataRegistry
+        If serverUrl.ToLower.StartsWith("http://") OrElse serverUrl.ToLower.StartsWith("https://") Then
+            Return New RegistryUrl(serverUrl)
+        Else
+            Return New RegistryUrl(RegistryUrl.defaultServer, serverUrl)
+        End If
+    End Function
+End Module
