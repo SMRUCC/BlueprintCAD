8fc1112a1b5ec772941699901933916f41ee8c07
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-29 20:36:05 +0800

    add table too view  the reaction in table view

--- src/BlueprintCAD/CellBrowser/ModelViewer/CellViewer.Designer.vb
+++ src/BlueprintCAD/CellBrowser/ModelViewer/CellViewer.Designer.vb
@@ -26,10 +26,19 @@ Partial Class CellViewer : Inherits DocumentWindow
         WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
         StatusStrip1 = New StatusStrip()
         ToolStripStatusLabel1 = New ToolStripStatusLabel()
-        ToolStripStatusLabel2 = New ToolStripStatusLabel()
         ToolStripStatusLabel3 = New ToolStripStatusLabel()
+        ToolStripStatusLabel2 = New ToolStripStatusLabel()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        AdvancedDataGridViewSearchToolBar1 = New Galaxy.Data.TableSheet.AdvancedDataGridViewSearchToolBar()
+        AdvancedDataGridView1 = New Galaxy.Data.TableSheet.AdvancedDataGridView()
         CType(WebView21, ComponentModel.ISupportInitialize).BeginInit()
         StatusStrip1.SuspendLayout()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
+        CType(AdvancedDataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' WebView21
@@ -38,18 +47,18 @@ Partial Class CellViewer : Inherits DocumentWindow
         WebView21.CreationProperties = Nothing
         WebView21.DefaultBackgroundColor = Color.White
         WebView21.Dock = DockStyle.Fill
-        WebView21.Location = New Point(0, 0)
+        WebView21.Location = New Point(3, 3)
         WebView21.Name = "WebView21"
-        WebView21.Size = New Size(1532, 862)
+        WebView21.Size = New Size(1518, 828)
         WebView21.TabIndex = 1
         WebView21.ZoomFactor = 1R
         ' 
         ' StatusStrip1
         ' 
         StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusLabel1, ToolStripStatusLabel3, ToolStripStatusLabel2})
-        StatusStrip1.Location = New Point(0, 862)
+        StatusStrip1.Location = New Point(3, 831)
         StatusStrip1.Name = "StatusStrip1"
-        StatusStrip1.Size = New Size(1532, 22)
+        StatusStrip1.Size = New Size(1518, 22)
         StatusStrip1.TabIndex = 2
         StatusStrip1.Text = "StatusStrip1"
         ' 
@@ -59,6 +68,12 @@ Partial Class CellViewer : Inherits DocumentWindow
         ToolStripStatusLabel1.Size = New Size(92, 17)
         ToolStripStatusLabel1.Text = "Select On Node:"
         ' 
+        ' ToolStripStatusLabel3
+        ' 
+        ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        ToolStripStatusLabel3.Size = New Size(67, 17)
+        ToolStripStatusLabel3.Text = "<Nothing>"
+        ' 
         ' ToolStripStatusLabel2
         ' 
         ToolStripStatusLabel2.IsLink = True
@@ -66,19 +81,74 @@ Partial Class CellViewer : Inherits DocumentWindow
         ToolStripStatusLabel2.Size = New Size(222, 17)
         ToolStripStatusLabel2.Text = "[Click To View The Neighborhood Graph]"
         ' 
-        ' ToolStripStatusLabel3
+        ' TabControl1
         ' 
-        ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
-        ToolStripStatusLabel3.Size = New Size(67, 17)
-        ToolStripStatusLabel3.Text = "<Nothing>"
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Dock = DockStyle.Fill
+        TabControl1.Location = New Point(0, 0)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(1532, 884)
+        TabControl1.TabIndex = 3
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(WebView21)
+        TabPage1.Controls.Add(StatusStrip1)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(1524, 856)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Network Viewer"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Controls.Add(AdvancedDataGridView1)
+        TabPage2.Controls.Add(AdvancedDataGridViewSearchToolBar1)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(1524, 856)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "Reaction Table"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' AdvancedDataGridViewSearchToolBar1
+        ' 
+        AdvancedDataGridViewSearchToolBar1.AllowMerge = False
+        AdvancedDataGridViewSearchToolBar1.GripStyle = ToolStripGripStyle.Hidden
+        AdvancedDataGridViewSearchToolBar1.Location = New Point(3, 3)
+        AdvancedDataGridViewSearchToolBar1.MaximumSize = New Size(0, 27)
+        AdvancedDataGridViewSearchToolBar1.MinimumSize = New Size(0, 27)
+        AdvancedDataGridViewSearchToolBar1.Name = "AdvancedDataGridViewSearchToolBar1"
+        AdvancedDataGridViewSearchToolBar1.RenderMode = ToolStripRenderMode.Professional
+        AdvancedDataGridViewSearchToolBar1.Size = New Size(1518, 27)
+        AdvancedDataGridViewSearchToolBar1.TabIndex = 0
+        AdvancedDataGridViewSearchToolBar1.Text = "AdvancedDataGridViewSearchToolBar1"
+        ' 
+        ' AdvancedDataGridView1
+        ' 
+        AdvancedDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        AdvancedDataGridView1.Dock = DockStyle.Fill
+        AdvancedDataGridView1.FilterAndSortEnabled = True
+        AdvancedDataGridView1.FilterStringChangedInvokeBeforeDatasourceUpdate = True
+        AdvancedDataGridView1.Location = New Point(3, 30)
+        AdvancedDataGridView1.MaxFilterButtonImageHeight = 23
+        AdvancedDataGridView1.Name = "AdvancedDataGridView1"
+        AdvancedDataGridView1.RightToLeft = RightToLeft.No
+        AdvancedDataGridView1.Size = New Size(1518, 823)
+        AdvancedDataGridView1.SortStringChangedInvokeBeforeDatasourceUpdate = True
+        AdvancedDataGridView1.TabIndex = 1
         ' 
         ' CellViewer
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1532, 884)
-        Controls.Add(WebView21)
-        Controls.Add(StatusStrip1)
+        Controls.Add(TabControl1)
         DockAreas = Microsoft.VisualStudio.WinForms.Docking.DockAreas.Float Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockLeft Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockRight Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockTop Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockBottom Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.Document
         DoubleBuffered = True
         Name = "CellViewer"
@@ -88,8 +158,13 @@ Partial Class CellViewer : Inherits DocumentWindow
         CType(WebView21, ComponentModel.ISupportInitialize).EndInit()
         StatusStrip1.ResumeLayout(False)
         StatusStrip1.PerformLayout()
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage1.PerformLayout()
+        TabPage2.ResumeLayout(False)
+        TabPage2.PerformLayout()
+        CType(AdvancedDataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
-        PerformLayout()
     End Sub
 
     Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
@@ -97,4 +172,9 @@ Partial Class CellViewer : Inherits DocumentWindow
     Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
     Friend WithEvents ToolStripStatusLabel3 As ToolStripStatusLabel
     Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents AdvancedDataGridView1 As Galaxy.Data.TableSheet.AdvancedDataGridView
+    Friend WithEvents AdvancedDataGridViewSearchToolBar1 As Galaxy.Data.TableSheet.AdvancedDataGridViewSearchToolBar
 End Class
