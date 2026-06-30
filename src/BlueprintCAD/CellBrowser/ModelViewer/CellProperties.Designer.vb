0b2a167f613101ca3f0bdd43671f975fd0e8dc13
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-30 11:42:12 +0800

    load cell propertises data

--- src/BlueprintCAD/CellBrowser/ModelViewer/CellProperties.Designer.vb
+++ src/BlueprintCAD/CellBrowser/ModelViewer/CellProperties.Designer.vb
@@ -24,9 +24,137 @@ Partial Class CellProperties
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        GroupBox3 = New GroupBox()
+        AdvancedDataGridView1 = New Galaxy.Data.TableSheet.AdvancedDataGridView()
+        AdvancedDataGridViewSearchToolBar1 = New Galaxy.Data.TableSheet.AdvancedDataGridViewSearchToolBar()
+        SplitContainer1 = New SplitContainer()
+        ListBox1 = New ListBox()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        CType(AdvancedDataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.Panel1.SuspendLayout()
+        SplitContainer1.Panel2.SuspendLayout()
+        SplitContainer1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Dock = DockStyle.Left
+        GroupBox1.Location = New Point(0, 0)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(412, 395)
+        GroupBox1.TabIndex = 1
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Cell Metadata"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(ListBox1)
+        GroupBox2.Dock = DockStyle.Fill
+        GroupBox2.Location = New Point(412, 0)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(488, 395)
+        GroupBox2.TabIndex = 2
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Cell Phenotype Traits"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(AdvancedDataGridView1)
+        GroupBox3.Controls.Add(AdvancedDataGridViewSearchToolBar1)
+        GroupBox3.Dock = DockStyle.Fill
+        GroupBox3.Location = New Point(0, 0)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(900, 392)
+        GroupBox3.TabIndex = 3
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Cell Environment I/O"
+        ' 
+        ' AdvancedDataGridView1
+        ' 
+        AdvancedDataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        AdvancedDataGridView1.Dock = DockStyle.Fill
+        AdvancedDataGridView1.FilterAndSortEnabled = True
+        AdvancedDataGridView1.FilterStringChangedInvokeBeforeDatasourceUpdate = True
+        AdvancedDataGridView1.Location = New Point(3, 46)
+        AdvancedDataGridView1.MaxFilterButtonImageHeight = 23
+        AdvancedDataGridView1.Name = "AdvancedDataGridView1"
+        AdvancedDataGridView1.RightToLeft = RightToLeft.No
+        AdvancedDataGridView1.Size = New Size(894, 343)
+        AdvancedDataGridView1.SortStringChangedInvokeBeforeDatasourceUpdate = True
+        AdvancedDataGridView1.TabIndex = 1
+        ' 
+        ' AdvancedDataGridViewSearchToolBar1
+        ' 
+        AdvancedDataGridViewSearchToolBar1.AllowMerge = False
+        AdvancedDataGridViewSearchToolBar1.GripStyle = ToolStripGripStyle.Hidden
+        AdvancedDataGridViewSearchToolBar1.Location = New Point(3, 19)
+        AdvancedDataGridViewSearchToolBar1.MaximumSize = New Size(0, 27)
+        AdvancedDataGridViewSearchToolBar1.MinimumSize = New Size(0, 27)
+        AdvancedDataGridViewSearchToolBar1.Name = "AdvancedDataGridViewSearchToolBar1"
+        AdvancedDataGridViewSearchToolBar1.RenderMode = ToolStripRenderMode.Professional
+        AdvancedDataGridViewSearchToolBar1.Size = New Size(894, 27)
+        AdvancedDataGridViewSearchToolBar1.TabIndex = 0
+        AdvancedDataGridViewSearchToolBar1.Text = "AdvancedDataGridViewSearchToolBar1"
+        ' 
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.Location = New Point(0, 0)
+        SplitContainer1.Name = "SplitContainer1"
+        SplitContainer1.Orientation = Orientation.Horizontal
+        ' 
+        ' SplitContainer1.Panel1
+        ' 
+        SplitContainer1.Panel1.Controls.Add(GroupBox2)
+        SplitContainer1.Panel1.Controls.Add(GroupBox1)
+        ' 
+        ' SplitContainer1.Panel2
+        ' 
+        SplitContainer1.Panel2.Controls.Add(GroupBox3)
+        SplitContainer1.Size = New Size(900, 791)
+        SplitContainer1.SplitterDistance = 395
+        SplitContainer1.TabIndex = 4
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.Dock = DockStyle.Fill
+        ListBox1.FormattingEnabled = True
+        ListBox1.Location = New Point(3, 19)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(482, 373)
+        ListBox1.TabIndex = 0
+        ' 
+        ' CellProperties
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(900, 791)
+        Controls.Add(SplitContainer1)
+        DockAreas = Microsoft.VisualStudio.WinForms.Docking.DockAreas.Float Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockLeft Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockRight Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockTop Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockBottom Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.Document
+        DoubleBuffered = True
+        Name = "CellProperties"
+        ShowHint = Microsoft.VisualStudio.WinForms.Docking.DockState.Unknown
+        TabPageContextMenuStrip = DockContextMenuStrip1
+        GroupBox2.ResumeLayout(False)
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        CType(AdvancedDataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.Panel1.ResumeLayout(False)
+        SplitContainer1.Panel2.ResumeLayout(False)
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents AdvancedDataGridView1 As Galaxy.Data.TableSheet.AdvancedDataGridView
+    Friend WithEvents AdvancedDataGridViewSearchToolBar1 As Galaxy.Data.TableSheet.AdvancedDataGridViewSearchToolBar
+    Friend WithEvents ListBox1 As ListBox
 End Class
--- src/BlueprintCAD/CellBrowser/ModelViewer/CellProperties.vb
+++ src/BlueprintCAD/CellBrowser/ModelViewer/CellProperties.vb
@@ -1,8 +1,53 @@
-﻿Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
+﻿Imports Galaxy.Data.TableSheet
+Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.GCModeller.Assembly.GCMarkupLanguage.v2
 
 Public Class CellProperties
 
-    Public Sub ShowModelProperties(cell As VirtualCell)
+    Dim loader As GridLoaderHandler
 
+    Public Sub ShowModelProperties(cell As VirtualCell)
+        Dim transports = cell.metabolismStructure.reactions.transportation.Keys.Indexing
+        Dim inputs As New List(Of String)
+        Dim outputs As New List(Of String)
+        Dim compoundIndex = cell.metabolismStructure.compounds.ToDictionary(Function(a) a.ID)
+
+        For Each rxn As Reaction In cell.metabolismStructure.reactions.AsEnumerable
+            Call inputs.AddRange(From m In rxn.substrate Select m.compound)
+            Call outputs.AddRange(From n In rxn.product Select n.compound)
+        Next
+        For Each id As String In cell.traits.phenotype.SafeQuery
+            Call ListBox1.Items.Add(id)
+        Next
+
+        Dim compounds = inputs _
+            .Select(Function(id) (id, "I")) _
+            .JoinIterates(outputs.Select(Function(id) (id, "O"))) _
+            .GroupBy(Function(i) i.id) _
+            .Where(Function(a) compoundIndex.ContainsKey(a.Key)) _
+            .Select(Function(i)
+                        Return (compoundIndex(i.Key), i.Select(Function(a) a.Item2).Distinct.OrderBy(Function(chr) chr).JoinBy("/"))
+                    End Function) _
+            .ToArray
+
+        Call loader.LoadTable(
+            Sub(tbl)
+                Call tbl.Columns.Add("Cell I/O", GetType(String))
+                Call tbl.Columns.Add("ID", GetType(String))
+                Call tbl.Columns.Add("name", GetType(String))
+                Call tbl.Columns.Add("formula", GetType(String))
+                Call tbl.Columns.Add("db_xrefs", GetType(String))
+
+                For Each c As (meta As Compound, io As String) In compounds
+                    Dim meta = c.meta
+
+                    Call tbl.Rows.Add(c.io, meta.ID, meta.name, meta.formula, meta.db_xrefs.JoinBy("; "))
+                Next
+            End Sub)
+    End Sub
+
+    Private Sub CellProperties_Load(sender As Object, e As EventArgs) Handles Me.Load
+        loader = New GridLoaderHandler(AdvancedDataGridView1, AdvancedDataGridViewSearchToolBar1)
     End Sub
 End Class
